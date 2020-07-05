58d7b784fcddc4fc3bc09edc7f8928c735239050
Nirmal Diaz <nirmaldiaz@gmail.com>
2020-07-05 21:20:31 +0530

    Changed: Extracted basic math methods to their own class

---
+++ src/BasicMath.mjs
@@ -0,0 +1,58 @@
+//@ts-check
+import { PrimeMath } from "./PrimeMath.mjs";
+
+export class BasicMath {
+    /** Returns all the integer factors of an integer
+     * @param {number} integer
+     * @return {number[]} All the integer factors of the integer
+    */
+    static getIntFactors(integer) {
+        const factors = [];
+        let factorRange = Math.sqrt(integer);
+        for (let i = 1; i <= factorRange; i++) {
+            if (integer % i === 0) {
+                factors.push(i);
+                factors.push(integer / i);
+            }
+        }
+        return factors;
+    }
+
+    /**
+     * Returns the factorial of an integer
+     * @param {number} integer
+     * @return {number} The factorial value of the integer
+     */
+    static getFactorial(integer) {
+        let factorialProduct = 1;
+        for (let i = 2; i <= integer; i++) {
+            factorialProduct = factorialProduct * i;
+        }
+        return factorialProduct;
+    }
+
+    /**
+         * Returns the greatest common divisor of 2 integers
+         * @param {number} integer1
+         * @param {number} integer2
+         * @return {number} The greatest common divisor of integer1 and integer2
+         */
+    static getGCD(integer1, integer2) {
+        if (integer2 === 0) {
+            return integer1;
+        } else {
+            return BasicMath.getGCD(integer2, integer1 % integer2);
+        }
+    }
+
+    /**
+     * Checks if an integer has no square factors other than 1
+     * @param {number} integer
+     * @return {boolean}
+     */
+    static isSquareFree(integer) {
+        const intPrimeFactorization = PrimeMath.getPrimeFactorization(integer);
+        const uniquePrimeFactors = new Set(intPrimeFactorization);
+        return intPrimeFactorization.length === uniquePrimeFactors.size;
+    }
+}
