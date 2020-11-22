d7e1d3a62315a2aa4acb57183f808617a2e92d10
Nirmal Diaz <nirmaldiaz@gmail.com>
2020-11-22 16:10:19 +0530

    Fixed: Wrong solution problem

--- src/SudokuPuzzle.mjs
+++ src/SudokuPuzzle.mjs
@@ -1,6 +1,6 @@
 //@ts-check
 export class SudokuPuzzle {
-    puzzle = [
+    tempPuzzle = [
         // [5,3,0,0,7,0,0,0,0],
         // [6,0,0,1,9,5,0,0,0],
         // [0,9,8,0,0,0,0,6,0],
@@ -12,6 +12,8 @@ export class SudokuPuzzle {
         // [0,0,0,0,8,0,0,7,9]
     ];
 
+    solvedPuzzle = [];
+
     constructor(puzzleString) {
         //NOTE: A puzzle string looks like follows
         //"530070000,600195000,098000060,800060003,400803001,700020006,060000280,000419005,000080079"
@@ -22,17 +24,17 @@ export class SudokuPuzzle {
         if (/(\d{9},){9}/.test(puzzleString)) {
             //Remove the trailing comma and split by "," to get row strings
             const rows = puzzleString.slice(0,-1).split(",");
-            this.puzzle = new Array(rows.length);
+            this.tempPuzzle = new Array(rows.length);
 
             //Convert each row string into a row of cells
             for (let r = 0; r < rows.length; r++) {
-                this.puzzle[r] = rows[r].split("");
+                this.tempPuzzle[r] = rows[r].split("");
             }
 
             //Parse each cell as integer
-            for (let r = 0; r < this.puzzle.length; r++) {
-                for (let c = 0; c < this.puzzle[r].length; c++) {
-                    this.puzzle[r][c] = Number.parseInt(this.puzzle[r][c]);
+            for (let r = 0; r < this.tempPuzzle.length; r++) {
+                for (let c = 0; c < this.tempPuzzle[r].length; c++) {
+                    this.tempPuzzle[r][c] = Number.parseInt(this.tempPuzzle[r][c]);
                 }
             }
         } else {
@@ -40,8 +42,8 @@ export class SudokuPuzzle {
         }
     }
 
-    getPuzzle() {
-        return this.puzzle;
+    getSolvedPuzzle() {
+        return this.solvedPuzzle;
     }
 
     /**
@@ -81,31 +83,33 @@ export class SudokuPuzzle {
     }
 
     solve() {
-        for (let r = 0; r < this.puzzle.length; r++) {
-            for (let c = 0; c < this.puzzle[r].length; c++) {
-                if (this.puzzle[r][c] === 0) {
+        for (let r = 0; r < this.tempPuzzle.length; r++) {
+            for (let c = 0; c < this.tempPuzzle[r].length; c++) {
+                if (this.tempPuzzle[r][c] === 0) {
                     //CASE: Cell is empty
                     for (let i = 1; i < 10; i++) {
-                        if (this.isCandidate(this.puzzle, r, c, i)) {
+                        if (this.isCandidate(this.tempPuzzle, r, c, i)) {
                             //CASE: Value is possible
                             //Place the value
-                            this.puzzle[r][c] = i;
+                            this.tempPuzzle[r][c] = i;
                             //Think the current state of the puzzle as a new puzzle and start solving over
                             this.solve();
-                            this.puzzle[r][c] = 0;
+                            this.tempPuzzle[r][c] = 0;
                         }
                     }
                     return;
                 }
             }
         }
+        //By now the puzzle is solved. Deep copy it
+        JSON.parse(JSON.stringify(this.tempPuzzle));
     }
 
     getStringifiedPuzzle() {
         let stringifiedPuzzle = "";
 
-        for (let r = 0; r < this.puzzle.length; r++) {
-            stringifiedPuzzle += this.puzzle[r].join("") + "\n";
+        for (let r = 0; r < this.solvedPuzzle.length; r++) {
+            stringifiedPuzzle += this.solvedPuzzle[r].join("") + "\n";
         }
 
         return stringifiedPuzzle;
